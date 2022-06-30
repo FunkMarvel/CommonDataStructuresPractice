713da3b0d805c73746f6609b76c6c17a10183222
potteplante <aanders35@gmail.com>
2022-06-30 22:42:46 +0200

    Finalized Stack implementation.

--- CommonDataStructuresPractice/StickyStack.h
+++ CommonDataStructuresPractice/StickyStack.h
@@ -6,12 +6,16 @@ template<typename T>
 class StickyStack{
 public:
     StickyStack();
+    explicit StickyStack(int StackCapacity);
+    StickyStack(int StackCapacity, T FirstElement);
     ~StickyStack();
 
     void CreateStack(int StackCapacity);
     void Push(T NewValue);
     T Pop();
     T Peek();
+    int GetSize();
+    int GetCapacity();
 
     [[nodiscard]] bool IsEmpty();
     [[nodiscard]] bool IsFull();
@@ -20,15 +24,14 @@ public:
 protected:
     T* Data{nullptr};
 
-    int Top{};
-    int Capacity{1};
     int Size{};
+    int Capacity{};
 
 };
 
 template<typename T>
 StickyStack<T>::StickyStack() {
-    CreateStack(Capacity);
+    CreateStack(4);
 }
 
 template<typename T>
@@ -39,17 +42,22 @@ StickyStack<T>::~StickyStack() {
 template<typename T>
 void StickyStack<T>::Push(T NewValue) {
     if (IsFull()) throw std::runtime_error("Stack overflow!");
-    Data[Top++] = NewValue;
+    Size++;
+    Data[Size-1] = NewValue;
 }
 
 template<typename T>
 T StickyStack<T>::Pop() {
-    return nullptr;
+    if(IsEmpty()) throw std::runtime_error("Stack underflow!");
+    T PoppedElement{Data[Size-1]};
+    Size--;
+    return PoppedElement;
 }
 
 template<typename T>
 T StickyStack<T>::Peek() {
-    return Data[Top];
+    if (IsEmpty()) throw std::runtime_error("Stack underflow!");
+    return Data[Size - 1];
 }
 
 template<typename T>
@@ -65,15 +73,38 @@ bool StickyStack<T>::IsFull() {
 template<typename T>
 void StickyStack<T>::CreateStack(int StackCapacity) {
     if (StackCapacity < 1) throw std::runtime_error("Stack Capacity must be positive, non-zero integer");
+    delete[] Data;
     Data = new T[StackCapacity];
-    Top = 0;
+    Capacity = StackCapacity;
+    Size = 0;
 }
 
 template<typename T>
 bool StickyStack<T>::Contains(T ElementToCheck) {
 
-    T* FoundElement = std::find(std::begin(Data), std::end(Data), ElementToCheck);
-    if (FoundElement != std::end(Data)) return true;
+    for (int i = 0; i < Size; ++i) {
+        if (Data[i] == ElementToCheck) return true;
+    }
 
     return false;
+}
+
+template<typename T>
+StickyStack<T>::StickyStack(int StackCapacity, T FirstElement) : StickyStack(StackCapacity) {
+    Push(FirstElement);
+}
+
+template<typename T>
+StickyStack<T>::StickyStack(int StackCapacity) {
+    CreateStack(StackCapacity);
+}
+
+template<typename T>
+int StickyStack<T>::GetSize() {
+    return Size;
+}
+
+template<typename T>
+int StickyStack<T>::GetCapacity() {
+    return Capacity;
 }
